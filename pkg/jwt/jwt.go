c56b84d073a7a4094c2dcf65051b47c46d00e90d
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-05-07 22:23:43 +0300

    remove public and private key files

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,50 +1,52 @@
 package jwt
 
 import (
+	"auth-service/pkg/config"
 	"auth-service/pkg/logging"
 	"crypto/rsa"
 	"github.com/dgrijalva/jwt-go"
 	"go.uber.org/zap"
-	"io/ioutil"
-	"log"
 	"time"
 )
 
-const (
-	PrivKey                    = "app_rsa" // openssl genrsa -out app.rsa keysize
-	Issuer                     = "info@thevpnbeast.com"
-	AccessTokenValidInMinutes  = 60
-	RefreshTokenValidInMinutes = 600
-)
-
 var (
-	signKey   *rsa.PrivateKey
+	signKey *rsa.PrivateKey
 	logger *zap.Logger
+	err error
+
+	privateKey, issuer string
+	accessTokenValidInMinutes, refreshTokenValidInMinutes int
 )
 
-
-// read the key files before starting http handlers
 func init() {
 	logger = logging.GetLogger()
 
-	signBytes, err := ioutil.ReadFile(PrivKey)
-	fatal(err)
+	issuer = config.GetStringEnv("ISSUER", "info@thevpnbeast.com")
+	privateKey = config.GetStringEnv("PRIVATE_KEY", "-----BEGIN PRIVATE KEY-----\nMIICdwIBADANBgkqhkiG" +
+		"9w0BAQEFAASCAmEwggJdAgEAAoGBANnmLifeLBsiXe/J\n8O3ophHHaCfJ+EdAUYn7vArJTUtankCD3I8O3n+QM0KNsXzXd+eN6VmNm3bjLp" +
+		"Hq\nVjI/jCr2m1EqXgvRQP74/wOU1sHN3zSRQbcPR0dfJiDfTRmfh/LVrKgcU0kQ4yrG\nlc0KGB2uslzrKLJCmQ4G0WeM3tKNAgMBAAECgY" +
+		"EApsep+FXzSGmLoOfegxqZUe5g\n6GOMp2yxfH2ztkXR5aVcj2DeRplI8DZ9Jamyei2p1xAl1aevoNXOZV0J0LgXHbm+\nP6MGU7d+IYD2hI" +
+		"CWPfD4pqJafkYc7Q94eQaIiShlYEOoEiLDt09m2V3J/VWxEWw0\nGTzT1T6zDuwD5epXY/kCQQDv+Xeq+SU5+avfysvm/8bITu/WBRXKxQ7V" +
+		"2dg9rJIF\nrAZSTPUIqdKm2F+o8DIX4sSMouFMgo81Ad4S8D13iQCPAkEA6HNRwmcfQCHzsuuT\n1407mEPFAcgIckU6e9ubXRRepWPjE6MJ" +
+		"IyrDeIkCJfgFPiK8OcNvFLUCD8NaySD1\nQuHRIwJAJtIqo8QOW6SiQ1/hQItcMwdiETNdZSIf1kSZkNCcBsLfeuzsLuyaIVeb\nkg7Za7fJ" +
+		"qB6pZ+EvHZohvNqUdwP4zQJBALf6piiG9C4PcVIYsOA3cYa3hNM/HqhK\n8NodW9+VAsBGyfC95rqF20aosiGZJ5UhavcRHvc1uNb/GPj99A" +
+		"EmuB8CQCy9M89/\nWGs0V60TrOWn2cmNlvexvxJgtWIjzdtp5rBj/E7Dmfx9nE6sG+uJqob389HYb0fF\nj8MrN6RCirNhupc=\n-----END" +
+		" PRIVATE KEY-----")
+	accessTokenValidInMinutes = config.GetIntEnv("ACCESS_TOKEN_VALID_IN_MINUTES", 60)
+	refreshTokenValidInMinutes = config.GetIntEnv("REFRESH_TOKEN_VALID_IN_MINUTES", 600)
 
+	signBytes := []byte(privateKey)
 	signKey, err = jwt.ParseRSAPrivateKeyFromPEM(signBytes)
-	fatal(err)
-}
-
-func fatal(err error) {
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
 
 func GenerateAccessToken(username string) (string, error) {
 	t := jwt.New(jwt.GetSigningMethod("RS256"))
 	t.Claims = &jwt.StandardClaims{
-		ExpiresAt: time.Now().Add(time.Minute * AccessTokenValidInMinutes).Unix(),
-		Issuer:    Issuer,
+		ExpiresAt: time.Now().Add(time.Duration(int32(accessTokenValidInMinutes)) * time.Minute).Unix(),
+		Issuer:    issuer,
 		Subject:   username,
 		IssuedAt:  time.Now().Unix(),
 	}
@@ -61,8 +63,8 @@ func GenerateAccessToken(username string) (string, error) {
 func GenerateRefreshToken(username string) (string, error) {
 	t := jwt.New(jwt.GetSigningMethod("RS256"))
 	t.Claims = &jwt.StandardClaims{
-		ExpiresAt: time.Now().Add(time.Minute * RefreshTokenValidInMinutes).Unix(),
-		Issuer:    Issuer,
+		ExpiresAt: time.Now().Add(time.Duration(int32(refreshTokenValidInMinutes)) * time.Minute).Unix(),
+		Issuer:    issuer,
 		Subject:   username,
 		IssuedAt:  time.Now().Unix(),
 	}
