c420b723821e1f7362db2f7d625739365e80c49c
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-05-15 14:44:00 +0300

    add constants

--- pkg/web/handler.go
+++ pkg/web/handler.go
@@ -31,13 +31,12 @@ func authenticateHandler() gin.HandlerFunc {
 		}
 
 		selectRes := selectResult{}
-		sqlStatement := fmt.Sprintf("SELECT encrypted_password, user_name FROM users WHERE user_name='%s'",
-			authReq.Username)
+		sqlStatement := fmt.Sprintf(SqlSelectUsernamePass, authReq.Username)
 		row := db.QueryRow(sqlStatement)
 		switch err := row.Scan(&selectRes.EncryptedPassword, &selectRes.UserName); err {
 		case sql.ErrNoRows:
 			logger.Warn("no rows were returned!", zap.String("user", authReq.Username))
-			errorResponse(context, http.StatusBadRequest, "User not found!")
+			errorResponse(context, http.StatusBadRequest, ErrUserNotFound)
 			context.Abort()
 			return
 		case nil:
@@ -49,7 +48,7 @@ func authenticateHandler() gin.HandlerFunc {
 			encryptRes, err := encryptReq.encrypt(authReq.Password, selectRes.EncryptedPassword)
 			if err != nil {
 				logger.Error("an error occurred while making encryption request", zap.String("error", err.Error()))
-				errorResponse(context, http.StatusInternalServerError, "unknown error occurred at the backend")
+				errorResponse(context, http.StatusInternalServerError, ErrUnknown)
 				context.Abort()
 				return
 			}
@@ -59,7 +58,7 @@ func authenticateHandler() gin.HandlerFunc {
 				if err != nil {
 					logger.Error("an error occurred generating access token",
 						zap.String("error", err.Error()))
-					errorResponse(context, http.StatusInternalServerError, "Unknown error occurred at the backend!")
+					errorResponse(context, http.StatusInternalServerError, ErrUnknown)
 					context.Abort()
 					return
 				}
@@ -68,7 +67,7 @@ func authenticateHandler() gin.HandlerFunc {
 				if err != nil {
 					logger.Error("an error occurred generating refresh token",
 						zap.String("error", err.Error()))
-					errorResponse(context, http.StatusInternalServerError, "Unknown error occurred at the backend!")
+					errorResponse(context, http.StatusInternalServerError, ErrUnknown)
 					context.Abort()
 					return
 				}
@@ -78,22 +77,18 @@ func authenticateHandler() gin.HandlerFunc {
 					Format(time.RFC3339)
 				refreshTokenExpiresAt := time.Now().Add(time.Duration(refreshTokenValidInMinutes) * time.Minute).
 					Format(time.RFC3339)
-				updateStatement := fmt.Sprintf("UPDATE users SET version = version + 1, last_login='%v', "+
-					"access_token='%v', access_token_expires_at='%v', refresh_token='%s', refresh_token_expires_at='%v' "+
-					"WHERE user_name='%s'", lastLogin, accessToken, accessTokenExpiresAt, refreshToken, refreshTokenExpiresAt,
-					authReq.Username)
+				updateStatement := fmt.Sprintf(SqlUpdateUser, lastLogin, accessToken, accessTokenExpiresAt,
+					refreshToken, refreshTokenExpiresAt, authReq.Username)
 				_, err = db.Exec(updateStatement)
 				if err != nil {
 					logger.Warn("an error  occurred while updating db", zap.String("error", err.Error()))
-					errorResponse(context, http.StatusInternalServerError, "Unknown error occurred at the backend!")
+					errorResponse(context, http.StatusInternalServerError, ErrUnknown)
 					context.Abort()
 					return
 				}
 
 				authRes := authSuccessResponse{}
-				sqlStatement := fmt.Sprintf("SELECT uuid, id, encrypted_password, created_at, updated_at, version, "+
-					"user_name, email, last_login, enabled, email_verified, access_token, access_token_expires_at, "+
-					"refresh_token, refresh_token_expires_at FROM users WHERE user_name='%s'", authReq.Username)
+				sqlStatement := fmt.Sprintf(SqlSelectUserAll, authReq.Username)
 				row := db.QueryRow(sqlStatement)
 				switch err := row.Scan(&authRes.Uuid, &authRes.Id, &authRes.EncryptedPassword, &authRes.CreatedAt, &authRes.UpdatedAt,
 					&authRes.Version, &authRes.Username, &authRes.Email, &authRes.LastLogin, &authRes.Enabled, &authRes.EmailVerified,
@@ -109,14 +104,14 @@ func authenticateHandler() gin.HandlerFunc {
 				}
 			} else {
 				logger.Error("password validation failed")
-				errorResponse(context, http.StatusBadRequest, "Invalid password!")
+				errorResponse(context, http.StatusBadRequest, ErrInvalidPass)
 				context.Abort()
 				return
 			}
 
 		default:
 			logger.Error("unknown error", zap.String("error", err.Error()))
-			errorResponse(context, http.StatusInternalServerError, "unknown error occurred at the backend")
+			errorResponse(context, http.StatusInternalServerError, ErrUnknown)
 			context.Abort()
 			return
 		}
