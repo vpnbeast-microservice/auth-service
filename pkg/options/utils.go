d275370159081599de50ad333a1331afd31b7580
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-07-24 22:03:53 +0300

    generic way to bind envs

--- pkg/options/utils.go
+++ pkg/options/utils.go
@@ -38,22 +38,22 @@ func convertStringToInt(s string) int {
 }
 
 // unmarshalConfig creates a new *viper.Viper and unmarshalls the config into struct using *viper.Viper
-func unmarshalConfig(key string, value interface{}) error {
+func unmarshalConfig(key string, opts interface{}) error {
 	sub := viper.Sub(key)
 	sub.AutomaticEnv()
 	sub.SetEnvPrefix(key)
-	bindEnvs(sub)
-	return sub.Unmarshal(value)
+	bindEnvs(sub, opts)
+	return sub.Unmarshal(opts)
 }
 
 // bindEnvs takes *viper.Viper as argument and binds structs fields to environments variables to be able to override
 // them using environment variables at the runtime
-func bindEnvs(sub *viper.Viper) {
-	opts := AuthServiceOptions{}
-	fieldCount := reflect.TypeOf(opts).NumField()
+func bindEnvs(sub *viper.Viper, opts interface{}) {
+	elem := reflect.ValueOf(opts).Type().Elem()
+	fieldCount := elem.NumField()
 	for i := 0; i < fieldCount; i++ {
-		env := reflect.TypeOf(opts).Field(i).Tag.Get("env")
-		name := reflect.TypeOf(opts).Field(i).Name
+		env := elem.Field(i).Tag.Get("env")
+		name := elem.Field(i).Name
 		_ = sub.BindEnv(name, env)
 	}
 }
